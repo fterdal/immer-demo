f64f8f538902317d94604c1e034f30018b6e2905
Finn <finnterdal@gmail.com>
2020-02-24 16:37:42 -0600

    inertial kermit

---
+++ demo.js
@@ -0,0 +1,46 @@
+// Exploring Immer
+const { green, red } = require("chalk")
+const { produce } = require("immer")
+
+const originalObject = {
+  name: "Finn",
+  tasks: [
+    { id: 1, type: "small", text: "check email" },
+    { id: 2, type: "medium", text: "create new slideshow" },
+    { id: 3, type: "large", text: "terraform mars" }
+  ],
+  location: {
+    lat: 41.895266,
+    long: -87.6412237
+  }
+}
+
+const newObject = produce(originalObject, draft => {
+  draft.name = "Travis"
+})
+
+const areObjectsTheSame = originalObject === newObject
+const areNamesTheSame = originalObject.name === newObject.name
+const areTasksTheSame = originalObject.tasks === newObject.tasks
+
+console.log("------------")
+
+console.log(
+  areObjectsTheSame
+    ? green("✅ The objects ARE the same")
+    : red("🔴 The objects are NOT the same")
+)
+
+console.log(
+  areNamesTheSame
+    ? green("✅ The names ARE the same")
+    : red("🔴 The names are NOT the same")
+)
+
+console.log(
+  areTasksTheSame
+    ? green("✅ The tasks ARE the same")
+    : red("🔴 The tasks are NOT the same")
+)
+
+console.log("------------")
